a959391f71d2dde3d340aa615235f830804b32cd
vishnu jangid <vishnujangid2303@gmail.com>
2024-01-21 18:53:25 +0530

    latest branch

--- src/components/JoinUsBanner.jsx
+++ src/components/JoinUsBanner.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const JoinUsBanner = () => {
   return (
+<<<<<<< HEAD
     <div className='bg-[#c0d1dd] rounded-[32px] flex flex-col md:flex-row md:items-center lg:px-[128px] md:px-[50px] md:py-[70px] p-[36px] bg-no-repeat bg-contain bg-center relative lg:w-[70%] w-[95%] m-auto overflow-hidden xl:max-w-[1140px] xl:m-auto'>
       <img src="/banner.svg" alt="banner" className='absolute right-[-30%] md:right-[10%] top-[-10%] h-full' />
+=======
+    <div className='bg-[#c0d1dd] rounded-[32px] flex flex-col md:flex-row md:items-center lg:px-[128px] md:px-[50px] md:py-[70px] p-[36px] bg-no-repeat bg-contain bg-center relative lg:w-[70%] w-[95%] m-auto overflow-hidden'>
+      <img src="/banner_image.png" alt="banner" className='absolute right-[-30%] md:right-[10%] top-[-10%] h-full' />
+>>>>>>> d515336ba5c7dac31a4c3f494027bdb36aa391e6
       <div className='w-[98%] h-[93%] bg-[#2d89c8fc] absolute rounded-[32px] left-[1%] top-[3.5%] opacity-60 '></div>
       <span className='z-10 mb-[48px]'>
         <p className='text-lg font-bold uppercase text-white'>Fueling Dreams</p>
